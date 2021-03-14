7a11c26021ef746003ad38a2362ee9fb8b6abb4e
Michael Barrett <69733844+michaeljohnbarrett@users.noreply.github.com>
2021-03-14 00:15:39 +0000

    Update harbour-addbuy.ts

--- translations/harbour-addbuy.ts
+++ translations/harbour-addbuy.ts
@@ -2,10 +2,45 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>CoverPage</name>
+    <name>LoadingBudget</name>
     <message>
-        <source>Add
-Buy</source>
+        <source>AddBuy for YNAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading Budget Data.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Slow Connection...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Network Timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please choose a default budget and account. These settings can be changed later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please quit the app, check network settings, then reopen.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Budget Ready</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -16,7 +51,39 @@ Buy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>AddBuy</source>
+        <source>Budget Categories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recent Transactions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Payee</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cleared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Memo</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -26,11 +93,150 @@ Buy</source>
         <source>Recent Transactions</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cleared Balance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Working Balance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cleared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Working</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Payee</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inflow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Outflow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Categories</name>
+    <message>
+        <source>Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Budgeted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Activity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Balance</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
         <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Budgets & Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last 2 weeks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last 30 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last 90 days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All transactions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sort old to new</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Display working & cleared account balances</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AddBuy for YNAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A simple transaction-entry app for YNAB users on Sailfish OS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>By Michael J. Barrett</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Source code:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Feedback:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enjoy the app?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
