ef2f93659a315461a908d995c3acc35c98a124c2
Michael Barrett <69733844+michaeljohnbarrett@users.noreply.github.com>
2021-11-07 18:18:50 +0000

    v0.2.1
    Should now be a complete and ready-to-go translation file.

--- translations/harbour-addbuy.ts
+++ translations/harbour-addbuy.ts
@@ -4,6 +4,10 @@
 <context>
     <name>LoadingBudget</name>
     <message>
+        <source>Authorize AddBuy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>AddBuy for YNAB</source>
         <translation type="unfinished"></translation>
     </message>
@@ -37,10 +41,6 @@
     </message>
     <message>
         <source>Please quit the app, check network settings, then reopen.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Budget Ready</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -79,6 +79,18 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Inflow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Payer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inflow: Ready to Assign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Cleared</source>
         <translation type="unfinished"></translation>
     </message>
@@ -86,11 +98,23 @@
         <source>Memo</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Transaction Saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Transaction Not Saved - Authentication or formatting error</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Recent</name>
     <message>
         <source>Recent Transactions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -133,6 +157,14 @@
         <source>Outflow</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Unknown Error - Unable to Load Account Balances</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown Error - Unable to Load Transactions</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Categories</name>
@@ -172,11 +204,23 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Please select a default account, then quit and reopen app to complete switch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error Loading Accounts - Please Re-access Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Default Account</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recent Transactions</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -204,11 +248,39 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Display working & cleared account balances</source>
+        <source>Selected account overrides default</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>General</source>
+        <source>Display working & cleared balances</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cover Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Balance 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Balance 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Authorization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If 'Keep me logged in' was selected upon login, please instead revoke AddBuy's access from YNAB settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forget Access Key</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -220,23 +292,23 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>A simple transaction-entry app for YNAB users on Sailfish OS.</source>
+        <source>A simple transaction-entry app for YNAB on Sailfish OS.\n\nBy Michael J. Barrett\n\nVersion 0.2.1\nLicensed under GNU GPLv3\n\nApp icon by JSEHV @ GitHub. Thanks for the contribution!\n\nAddBuy for YNAB is an unofficial application and is in no way associated with You Need A Budget LLC.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>By Michael J. Barrett</source>
+        <source>by Michael J. Barrett</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Source code:</source>
+        <source>SOURCE</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Feedback:</source>
+        <source>FEEDBACK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Enjoy the app?</source>
+        <source>DONATE</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
